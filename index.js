d9c44c6282c7a99528ad36cadaaae52d4a30701f
Umer Farooq <umerfarooq@Umers-Mac-mini.lan>
2024-05-26 21:02:48 +0100

    a1 1

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.double = void 0;
-function double(a) {
-    return a * 2;
-}
-exports.double = double;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJzcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQUEsU0FBZ0IsTUFBTSxDQUFDLENBQVM7SUFDNUIsT0FBTyxDQUFDLEdBQUcsQ0FBQyxDQUFDO0FBQ2YsQ0FBQztBQUZILHdCQUVHIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGZ1bmN0aW9uIGRvdWJsZShhOiBudW1iZXIpOiBudW1iZXIge1xuICAgIHJldHVybiBhICogMjtcbiAgfSJdfQ==
