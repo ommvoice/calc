05af019c60ba4b223d7391733a1cd3bc43f0ba9f
Umer Farooq <umerfarooq@Umers-Mac-mini.lan>
2024-05-26 20:32:09 +0100

    slack and refactor

---
+++ dist/package/calc.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.double = void 0;
+function double(a) {
+    return a * 2;
+}
+exports.double = double;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2FsYy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9jYWxjLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLFNBQWdCLE1BQU0sQ0FBQyxDQUFTO0lBQzVCLE9BQU8sQ0FBQyxHQUFHLENBQUMsQ0FBQztBQUNmLENBQUM7QUFGSCx3QkFFRyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBmdW5jdGlvbiBkb3VibGUoYTogbnVtYmVyKTogbnVtYmVyIHtcbiAgICByZXR1cm4gYSAqIDI7XG4gIH0iXX0=
